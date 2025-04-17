a832037bb12acb41c395f0385afd20e219b7597e
mk1035953 <mk1035953@wcupa.edu>
2025-04-17 11:14:18 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,8 +3,18 @@ public class Test {
     private ArrayList<Email> spam;
     private ArrayList<Email> notSpam;
     
-    public Test(ArrayList<Email> training, ArrayList<Email> test){
-        
+    public Test(ArrayList<Email> training){
+        spam = new ArrayList<Email>();
+        notSpam = new ArrayList<Email>();
+
+        for(Email e:training){
+            if(e.getSpam()){
+                spam.add(e);
+            }
+            else{
+                notSpam.add(e);
+            }
+        }
     }
     
     public boolean uniqueWords(){
@@ -13,8 +23,33 @@ public class Test {
         
         return false;
     }
-    public boolean wordCount(){
-        
+    public boolean wordCount(Email mail){
+        double[] spamCounts = new double[spam.size()];
+        double[] notCounts = new double[notSpam.size()];
+
+        for(int i = 0; i<spam.size();i++){
+            spamCounts[i] = spam.get(i).getWordCount();
+        }
+        for(int i = 0; i<notSpam.size();i++){
+            notCounts[i] = notSpam.get(i).getWordCount();
+        }   
+
+        double spamAvg = DataAnalysis.avg(spamCounts);
+        double spamStdev = DataAnalysis.stdev(spamCounts);
+        double notAvg = DataAnalysis.avg(notCounts);
+        double notStdev = DataAnalysis.stdev(notCounts);
+
+        if((Math.abs(spamAvg-notAvg)<(spamStdev+notStdev))){
+            return false;
+        }
+        else{
+            int test = mail.getWordCount();
+
+            if(test<=(spamAvg+spamStdev)||test>=(spamAvg-spamStdev)){
+                return true;
+            }
+        }
+
         return false;
     }
     public boolean doTests(Email mail){
