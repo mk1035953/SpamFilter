0361fcd28860202262709aa3bbea80530a2060a0
mk1035953 <mk1035953@wcupa.edu>
2025-04-17 10:13:59 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,9 +3,12 @@ public class Test {
     public Test(ArrayList<Email> training, ArrayList<Email> test){
         
     }
-    public boolean wordCount(){
+    public boolean uniqueWords(){
         return false;
-
+    }
+    public boolean wordCount(){
+        
+        return false;
     }
     public boolean doTests(Email mail){
         return false;
